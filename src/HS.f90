9f33a5d9f4d4d75625c1e1da97d368911c0d248a
paramecium <zxc22762246@gmail.com>
2026-06-02 02:31:08 +0800

    [Add] Delta phi, Excess entropy

--- src/HS.f90
+++ src/HS.f90
@@ -4,7 +4,9 @@ program HS_Network
     integer :: N, nstep, npast, NSTEP_PROTOCOL, nedge, nskip_edge
     integer :: LWORK, LWORK2, LDA, LDVL, LDVR
     integer :: nreal, iseed, idir, ntau, na, nb, NN
-    real(8) :: xKx1, xKx2, HS_Y, HS_Y_avg, EXP_HS_Y_avg
+    real(8) :: xKx1, xKx2, HS_Y, HS_Y_avg, EXP_HS_Y_avg, Delta_phi
+    real(8) :: phi_start, phi_end, Delta_phi_avg, entropy_ex, entropy_hk
+    real(8) :: heat_total, heat_total_avg, entropy_total, entropy_total_avg
     real(8) :: tt, tpast, protocol, protocol_next    
     real(8) :: dum, time1, time2, time1_HS, time2_HS
     real(8) :: DD, dt, prob, W_mean, W_sig, r0, w0, period, tau, bb
@@ -16,7 +18,7 @@ program HS_Network
     real(8), allocatable :: inweightdeg(:), outweightdeg(:)
 
     ! dynamic variables
-    real(8), allocatable :: f1(:), x1(:), noise_increment(:), fluct(:), fluct_prev(:)!, xt(:,:), ft(:,:)
+    real(8), allocatable :: f1(:), x1(:), noise_increment(:), fluct(:), fluct_prev(:), x_prev(:), f2(:), dq_step(:)!, xt(:,:), ft(:,:)
     real(8), allocatable :: f_avg(:,:), x_avg(:,:), K_avg(:,:,:)
     real(8), allocatable :: f_real(:,:), x_real(:,:)
     real(8), allocatable :: dxi(:), dqi(:), dsi(:), dwi(:)
@@ -50,7 +52,7 @@ program HS_Network
     allocate(a(N), r(N), noise(N,N), G(N,N), Q(N,N))
     allocate(inweightdeg(N), outweightdeg(N))
 
-    allocate(f1(N), x1(N), noise_increment(N), fluct(N), fluct_prev(N))
+    allocate(f1(N), x1(N), noise_increment(N), fluct(N), fluct_prev(N), x_prev(N), f2(N), dq_step(N))
     allocate(dxi(N), dqi(N), dsi(N), dwi(N))
     allocate(f_avg(N, -NPAST:NSTEP), x_avg(N, -NPAST:NSTEP), K_avg(N, N, -NPAST:NSTEP))
     allocate(f_real(N, -NPAST:NSTEP), x_real(N, -NPAST:NSTEP))
@@ -59,6 +61,8 @@ program HS_Network
     allocate(QschurBWORK(LWORK2))
     allocate(QschurTMP(N,N), QschurC(N,N), QschurSol(N,N), KoT(N,N), ans(N,N))
     allocate(Ko_inv_protocol(N,N,0:NSTEP_PROTOCOL), logdet_K(0:NSTEP_PROTOCOL))
+
+    allocate(heat_sim(N), entropy_sim(N), work_sim(N), energy_sim(N))
 
     OPEN(UNIT=11, FILE='config/G0_connect.csv', STATUS='REPLACE')
 	OPEN(UNIT=12, FILE='config/G_connect.csv', STATUS='REPLACE')	     
@@ -71,6 +75,8 @@ program HS_Network
     OPEN(UNIT=18, FILE='output/K.dat', STATUS='REPLACE')
     OPEN(UNIT=19, FILE='output/Ko.dat', STATUS='REPLACE')
     OPEN(UNIT=20, FILE='output/solution.dat', STATUS='REPLACE')
+    OPEN(UNIT=21, FILE='output/heat.dat', STATUS='REPLACE')
+    OPEN(UNIT=22, FILE='output/entropy_env.dat', STATUS='REPLACE')
 
     G = 0d0; G0 = 0
 	!random graph
@@ -151,6 +157,7 @@ program HS_Network
     !xt = 0; ft = 0
     x_avg = 0; f_avg = 0; K_avg = 0
     dxi = 0; dqi = 0; dsi = 0; dwi = 0
+    heat_sim = 0d0; entropy_sim = 0d0; work_sim = 0d0; energy_sim = 0d0
     do k = 1, nreal
         if (mod(k, int(nreal*1./10)) == 1) call progress(k, nreal)
         protocol = 0; protocol_next = 0; x1 = 0; f1 = 0; noise_increment = 0
@@ -208,12 +215,21 @@ program HS_Network
     call cpu_time(time1_HS)
     ! HATANO-SASA
     xKx1 = 0d0; xKx2 = 0d0
-    HS_Y_avg = 0d0; EXP_HS_Y_avg = 0d0
+    HS_Y_avg = 0d0; EXP_HS_Y_avg = 0d0; Delta_phi_avg = 0d0
+    heat_total_avg = 0d0; entropy_total_avg = 0d0
     do k = 1, nreal
-        HS_Y = 0d0
+        HS_Y = 0d0; phi_start = 0d0; phi_end = 0d0; Delta_phi = 0d0
+        heat_total = 0d0; entropy_total = 0d0
         protocol = 0; protocol_next = 0; x1 = 0; f1 = 0; noise_increment = 0
+        dqi = 0d0; dsi = 0d0
 
         do ii = -npast, NSTEP_PROTOCOL
+            if (ii == 0) then
+                fluct = x1 - x_avg(:, 0)
+                xKx1 = DOT_PRODUCT(fluct, MATMUL(Ko_inv_protocol(:,:,0), fluct))
+                phi_start = 0.5d0*logdet_K(0) + 0.5d0*xKx1
+            endif
+
             if (ii >= 1) then
                 fluct = x1 - x_avg(:, ii) ! fluctuation at time step ii
                 fluct_prev = x1 - x_avg(:, ii-1)
@@ -222,7 +238,13 @@ program HS_Network
                 HS_Y = HS_Y + 0.5d0*(logdet_K(ii) - logdet_K(ii-1)) + 0.5d0*(xkx1 - xkx2)
             endif
 
-            if (ii == NSTEP_PROTOCOL) exit ! no need to advance state at last protocol step
+            if (ii == NSTEP_PROTOCOL) then
+                fluct = x1 - x_avg(:, NSTEP_PROTOCOL)
+                xKx1 = DOT_PRODUCT(fluct, MATMUL(Ko_inv_protocol(:,:,NSTEP_PROTOCOL), fluct))
+                phi_end = 0.5d0*logdet_K(NSTEP_PROTOCOL) + 0.5d0*xKx1
+                Delta_phi = phi_end - phi_start
+                exit ! no need to advance state at last protocol step
+            endif
 
             tt = ii*dt
             do i = 1, N
@@ -230,17 +252,48 @@ program HS_Network
             enddo
             call protocol_at_time(tt, tau, bb, protocol)
             call protocol_at_time(tt + dt, tau, bb, protocol_next)
+            x_prev = x1
             call advance_hs_state_heun(N, r, a, G, protocol, protocol_next, noise_increment, f1, x1)
+            if (ii >= 0 .and. ii < NSTEP_PROTOCOL) then
+                call calculate_hs_force(N, r, a, G, protocol_next, x1, f2)
+                dq_step = -0.5d0*(f1 + f2)*(x1 - x_prev)
+                dqi = dqi + dq_step
+                do i = 1, N
+                    dsi(i) = dsi(i) + 2d0/noise(i,i)*dq_step(i)
+                enddo
+            endif
         enddo
         HS_Y_avg = HS_Y_avg + HS_Y
-        EXP_HS_Y_avg = EXP_HS_Y_avg + exp(HS_Y)
+        EXP_HS_Y_avg = EXP_HS_Y_avg + exp(-HS_Y)
+        Delta_phi_avg = Delta_phi_avg + Delta_phi
+        heat_sim = heat_sim + dqi
+        entropy_sim = entropy_sim + dsi
+        heat_total = sum(dqi)
+        entropy_total = sum(dsi)
+        heat_total_avg = heat_total_avg + heat_total
+        entropy_total_avg = entropy_total_avg + entropy_total
     enddo
     call cpu_time(time2_HS)
     HS_Y_avg = HS_Y_avg/nreal
     EXP_HS_Y_avg = EXP_HS_Y_avg/nreal
+    Delta_phi_avg = Delta_phi_avg/nreal
+    heat_sim = heat_sim/nreal
+    heat_total_avg = heat_total_avg/nreal
+    entropy_sim = entropy_sim/nreal
+    entropy_total_avg = entropy_total_avg/nreal
     write(*,*) 'CPU time for Hatano-Sasa: ', time2_HS - time1_HS, ' seconds.'
     write(6,*) 'Hatano-Sasa Y average = ', HS_Y_avg
     write(6,*) 'Hatano-Sasa Y exponential average = ', EXP_HS_Y_avg
+    write(6,*) 'Delta phi average = ', Delta_phi_avg
+    write(6,*) 'Excess Entropy average = ', HS_Y_avg - Delta_phi_avg
+    ! write(6,*) 'Housekeeping Entropy average = ', Delta_phi_avg - HS_Y_avg 
+    write(6,*) 'Heat total average = ', heat_total_avg
+    write(6,*) 'Environment entropy average = ', entropy_total_avg
+
+    do i = 1, N
+        write(21,*) i, heat_sim(i)
+        write(22,*) i, entropy_sim(i)
+    enddo
 
     call protocol_at_time(0d0, tau, bb, protocol)
     call build_jacobian_from_protocol(N, r, G, protocol, Q)
